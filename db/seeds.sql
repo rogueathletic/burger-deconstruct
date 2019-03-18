df9f3fd2c0a147a84cb61ff3beb651f0eeb468c2
Jason Schutz <jason@rogueathletic.com>
2019-03-17 20:25:59 -0700

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegi Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pokey Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tuna Burger', FALSE);
