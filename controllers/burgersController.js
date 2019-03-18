700c48c88a1a8261b0fe7869342082529b8f9739
markring <mark@boldtech.co>
2019-03-17 21:22:27 -0700

    import update

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
 var router = express.Router();
-var burger = require("../models/burger.js");
+var db = require("../models");
 
 // get route -> index
 router.get("/", function(req, res) {
@@ -10,7 +10,7 @@ router.get("/", function(req, res) {
 
 router.get("/burgers", function(req, res) {
   // express callback response by calling burger.selectAllBurger
-  burger.all(function(burgerData) {
+  db.burger.findAll(function(burgerData) {
     // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
     res.render("index", { burger_data: burgerData });
   });
@@ -19,7 +19,7 @@ router.get("/burgers", function(req, res) {
 // post route -> back to index
 router.post("/burgers/create", function(req, res) {
   // takes the request object using it as input for burger.addBurger
-  burger.create(req.body.burger_name, function(result) {
+  db.burger.create(req.body.burger_name, function(result) {
     // wrapper for orm.js that using MySQL insert callback will return a log to console,
     // render back to index with handle
     console.log(result);
