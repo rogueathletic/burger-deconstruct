4805821f3ef46e80d10b81ab96e3f5250d43041c
markring <mark@boldtech.co>
2019-03-17 21:32:29 -0700

    removed mysql

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
 var router = express.Router();
-var db = require("../models/burger.js");
+var db = require("../models");
 
 // get route -> index
 router.get("/", function(req, res) {
