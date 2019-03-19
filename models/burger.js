05f29aebcc64b85af86dff7810a22484dd2b7899
Jason Schutz <jason@rogueathletic.com>
2019-03-18 21:56:40 -0700

    punctuation

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,7 @@
 
 module.exports = (sequelize, DataTypes) => {
   var Burger = sequelize.define('Burger', {
-    burger_name: DataTypes.STRING,
+    Burger_name: DataTypes.STRING,
     devoured: DataTypes.BOOLEAN
   }, {});
   Burger.associate = function(models) {
