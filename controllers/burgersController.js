c4f3e64e359f2956a068391e563f007ee45775ba
Jason Schutz <jason@rogueathletic.com>
2019-03-19 21:26:31 -0700

    more

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -20,7 +20,7 @@ router.get("/", function(req, res) {
   });
 
   // <----  POST route for saving a new bgers. We can create a bgers using the data on req.body
-  router.post("/burgers", function (req, res) {
+  router.post("/burgers/create", function (req, res) {
       db.Burger.create({
         burger_name: req.body.burger_name
     }).then(function (dbBurgers) {
